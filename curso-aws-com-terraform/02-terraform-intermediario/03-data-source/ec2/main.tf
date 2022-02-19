0bcf141d3cc5a06f2fcef75feaa50207365f9994
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-02-19 20:51:03 -0300

    update to linux

--- curso-aws-com-terraform/02-terraform-intermediario/03-data-source/ec2/main.tf
+++ curso-aws-com-terraform/02-terraform-intermediario/03-data-source/ec2/main.tf
@@ -1,19 +1,17 @@
 terraform {
-  required_version = "0.14.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.23.0"
     }
   }
 
-  backend "s3" {
-    bucket  = "tfstate-968339500772"
-    key     = "dev/03-data-sources-s3/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "playground"
-  }
+ # backend "s3" {
+ #   bucket  = "tfstate-812498273566"
+ #   key     = "dev/03-data-sources-s3/terraform.tfstate"
+ #   region  = "us-east-1"
+ #   profile = "playground"
+ # }
 }
 
 provider "aws" {
